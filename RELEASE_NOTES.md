74ae361ef397c56ee870872217c791b2700dac85
Ignacio Corderi <icorderi@msn.com>
2015-04-08 18:01:51 -0700

    Ready for 0.2.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,5 @@
+#### 0.2.0 - April 08 2015
+* Fixed release scripts
+
 #### 0.1 - April 08 2015
 * Initial release
--- src/KineticMonitor/AssemblyInfo.fs
+++ src/KineticMonitor/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("KineticMonitor")>]
 [<assembly: AssemblyProductAttribute("KineticMonitor")>]
 [<assembly: AssemblyDescriptionAttribute("A monitor service for Kinetic devices")>]
-[<assembly: AssemblyVersionAttribute("0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.1")>]
+[<assembly: AssemblyVersionAttribute("0.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.1"
+    let [<Literal>] Version = "0.2.0"
